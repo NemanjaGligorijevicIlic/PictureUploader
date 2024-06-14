43d82c91e5b7c23518ef2650ab49a916bebfea12
peco-tina <nemgli-8@student.ltu.se>
2024-06-14 11:15:29 +0200

    Picture uploader to the cloud

--- src/main/java/org/example/S3ClientCreator.java
+++ src/main/java/org/example/S3ClientCreator.java
@@ -7,16 +7,16 @@ import software.amazon.awssdk.services.s3.S3Client;
 
 
 public class S3ClientCreator {
-    private final String accessKey = "AKIAV3OF5WXD7UVJF7PH";
-    private final String secretKey = "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";
-    
+    private final String accessKey = "";
+    private final String secretKey = "";
+
     public S3Client createS3Client(){
         Region region = Region.US_EAST_1;
         S3Client s3Client = S3Client.builder()
                 .region(region)
                 .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey)))
                 .build();
-        
+
         return s3Client;
     }
 }
