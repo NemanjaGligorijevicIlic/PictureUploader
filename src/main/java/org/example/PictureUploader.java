3ed797893e39beffe61acdd18d1cbdd2617e7f21
peco-tina <nemgli-8@student.ltu.se>
2024-06-14 11:27:11 +0200

    Picture uploader to the cloud

--- src/main/java/org/example/PictureUploader.java
+++ src/main/java/org/example/PictureUploader.java
@@ -1,11 +1,7 @@
 package org.example;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
-import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.*;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
--- src/main/java/org/example/S3ClientCreator.java
+++ src/main/java/org/example/S3ClientCreator.java
@@ -7,14 +7,14 @@ import software.amazon.awssdk.services.s3.S3Client;
 
 
 public class S3ClientCreator {
-    private final String accessKey = "";
-    private final String secretKey = "";
+    private final String ACCESS_KEY = "";
+    private final String SECRET_KEY = "";
 
     public S3Client createS3Client(){
         Region region = Region.US_EAST_1;
         S3Client s3Client = S3Client.builder()
                 .region(region)
-                .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey)))
+                .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(ACCESS_KEY, SECRET_KEY)))
                 .build();
 
         return s3Client;
